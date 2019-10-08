5a526059f335d853e58ac6a008aceb6ec06b66df
WaRRenGYJ <warrengyj@163.com>
2019-10-08 14:44:06 +0800

    图片编码转换第一版本提交(修改)

--- PIL_cv2_base64/PIL_base64.py
+++ PIL_cv2_base64/PIL_base64.py
@@ -1,3 +1,6 @@
+
+import base64
+from io import BytesIO
 def pil_2_base64(image):
     img_buffer = BytesIO()
     image.save(img_buffer, format='JPEG')
@@ -8,12 +11,16 @@ def pil_2_base64(image):
 
 import base64
 from io import BytesIO
-
 from PIL import Image
-
-
 def base64_2_pil(base64_str):
     image = base64.b64decode(base64_str)
     image = BytesIO(image)
     image = Image.open(image)
     return image
+
+
+
+
+
+
+
--- PIL_cv2_base64/PIL_cv2.py
+++ PIL_cv2_base64/PIL_cv2.py
@@ -1,6 +1,7 @@
+
+import cv2
+from PIL import Image
 import numpy as np
-
-
 def PIL_2_cv2(img_path):
     image = Image.open(img_path)
     img = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
@@ -9,9 +10,13 @@ def PIL_2_cv2(img_path):
 
 import cv2
 from PIL import Image
-
-
 def cv2_2_PIL(img_path):
     image = cv2.imread(img_path)
     img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     return img
+
+
+
+
+
+
